10d6b054973eda4929415158c8e590117b94af28
Joshua <torres_306@yahoo.com>
2021-04-01 19:52:49 -0400

    removing the disables eslint

--- src/components/table.js
+++ src/components/table.js
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Table from 'react-bootstrap/Table';
